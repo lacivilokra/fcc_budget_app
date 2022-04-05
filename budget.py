44c6a34296bc25c766d98b0c91568a8863e21813
lacivilokra <100162393+lacivilokra@users.noreply.github.com>
2022-04-05 15:15:49 -0400

    Add files via upload

--- budget.py
+++ budget.py
@@ -12,13 +12,43 @@ class Category:
         self.ledger.append(depo)
 
     def withdraw(self, amount, description = ''):
-        self.description = description
-        withdraw_funds = amount * -1
-        depo = {'amount' : withdraw_funds, 'description': self.description}
-        self.ledger.append(depo)
+        if self.check_funds(amount) == False:
+            return False
+        else:
+            self.description = description
+            withdraw_funds = amount * -1
+            depo = {'amount' : withdraw_funds, 'description': self.description}
+            self.ledger.append(depo)
+            self.balance += withdraw_funds
+            self.withdraw_funds += withdraw_funds
+            return True
 
-    def get_balance():
-        return(self.balance)
+    def get_balance(self):
+        return self.balance
+
+    def transfer(self, amount, category):
+        if self.check_funds(amount) == False:
+            return False
+        else:
+            self.withdraw(amount, 'Transfer to {}'.format(category.name))
+            self.deposit(amount, 'Transfer from {}'.format(self.name))
+            return True
+
+    def check_funds(self, amount):
+        if amount > self.balance:
+            return False
+        else:
+            return True
+
+    def __str__(self):
+        title = self.name.center(30, '*')
+
+def create_spend_chart()
+
+
+
+
+
 
 
         
